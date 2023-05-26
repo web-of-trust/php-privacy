98e3066254d851b3133e7147b505b082e362777b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2023-05-26 08:34:46 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Message/LiteralMessageTest.php
@@ -0,0 +1,146 @@
+<?php declare(strict_types=1);
+
+namespace OpenPGP\Tests\Message;
+
+use OpenPGP\Key\PublicKey;
+use OpenPGP\Message\LiteralMessage;
+use OpenPGP\Message\Signature;
+use OpenPGP\Packet\LiteralData;
+use OpenPGP\Tests\OpenPGPTestCase;
+
+/**
+ * Testcase class for OpenPGP literal message.
+ */
+class LiteralMessageTest extends OpenPGPTestCase
+{
+    const LITERAL_TEXT = 'Hello PHP PG';
+
+    public function testVerifyRsaSignedMessage()
+    {
+        $messageData = <<<EOT
+-----BEGIN PGP MESSAGE-----
+
+owEBWgGl/pANAwAIARhNDcT1xTKyAcsTYgBkcAjISGVsbG8gUEhQIFBHCokBMwQA
+AQgAHRYhBPxQBN+UcydxB+qmBRhNDcT1xTKyBQJkcAjIAAoJEBhNDcT1xTKyD5IH
+/Rjx4qKJyRkH0g+Gn5XEBFp/HlahltPeYAWzRAhgEPHfCtHLg4MjYRN4QnyqwLTq
+U2bICqzD3x0QIYdS5Z9fkAmtIO9ZsnJvv9XYdrxIMCrePkKM7sT/l31M0sFHvowv
+lPvb9DSkbM57zP/sd16CjHBVVARD4g330BQ4aoXL9T1ngoXqAV7iZdQSxT2hmAwf
+I5qDjicvANuKyIfE5k+Zsg4k6mnSsDv+D0vGt8cOx6OgY1j4E9XK4ipNdp2dTt5J
+canceovt1cRU7VUluanqpAc5eOecodUL6LaY2LZEjnFZWjw7BP2eCh0FZKx8Og5U
+koIAxNXOxaRF4vybpbI7FWQ=
+=/rPl
+-----END PGP MESSAGE-----
+EOT;
+
+        $publicKey = PublicKey::fromArmored(
+            file_get_contents('tests/Data/RsaPublicKey.asc')
+        );
+        $message = LiteralMessage::fromArmored($messageData);
+        $this->assertSame(self::LITERAL_TEXT, trim($message->getLiteralData()->getData()));
+        $this->assertTrue($message->getSignature() instanceof Signature);
+
+        $verification = $message->verify([$publicKey])[0];
+        $this->assertSame('184d0dc4f5c532b2', $verification->getKeyID(true));
+        $this->assertTrue($verification->isVerified());
+    }
+
+    public function testVerifyDsaSignedMessage()
+    {
+        $messageData = <<<EOT
+-----BEGIN PGP MESSAGE-----
+
+owGbwMvMwCH4eKNsipLHgkmMp4WTGFIKuHs9UnNy8hUCPAIUAty5OkpZGAQ5GGTF
+FFnswifaxuecvj1B6fs9mCZWJpAOBi5OAZjIy3UM/+M4F12pX+B0JoE/qqiBM697
+34NPPIrd01an7ZU5cDHm7mWGf1o/NXhbri9adtLvwvm5VxcXSfl82xFgtnmFlJ1u
+4L2Xjy8DAA==
+=/m2m
+-----END PGP MESSAGE-----
+EOT;
+
+        $publicKey = PublicKey::fromArmored(
+            file_get_contents('tests/Data/DsaPublicKey.asc')
+        );
+        $message = LiteralMessage::fromArmored($messageData);
+        $this->assertSame(self::LITERAL_TEXT, trim($message->getLiteralData()->getData()));
+        $this->assertTrue($message->getSignature() instanceof Signature);
+
+        $verification = $message->verify([$publicKey])[0];
+        $this->assertSame('e3b11d642248a092', $verification->getKeyID(true));
+        $this->assertTrue($verification->isVerified());
+    }
+
+    public function testVerifyEcP384SignedMessage()
+    {
+        $messageData = <<<EOT
+-----BEGIN PGP MESSAGE-----
+
+owGbwMvMwCm8ienmo1e3XHgYTwsnMaQUcP/0SM3JyVcI8AhQCHDn6pjKwiDMySAr
+psjCeqDVU1fkJ2fZ80PbYJpYmUA6GLg4BWAiO5YzNhwouNc15yt7gfLVVx4CtQ8S
+v+6aFjDPdbvdflERe4fNhsX72z+Y9qemeK0wkPz7+MiZi4wNbR0qxnMcM2/1fi2e
+olN2PbFm8ibP8kmmPQc+OM3z+u23f89FNfb9Lxr1/As0POP/HcoBAA==
+=xVkT
+-----END PGP MESSAGE-----
+EOT;
+
+        $publicKey = PublicKey::fromArmored(
+            file_get_contents('tests/Data/EcP384PublicKey.asc')
+        );
+        $message = LiteralMessage::fromArmored($messageData);
+        $this->assertSame(self::LITERAL_TEXT, trim($message->getLiteralData()->getData()));
+        $this->assertTrue($message->getSignature() instanceof Signature);
+
+        $verification = $message->verify([$publicKey])[0];
+        $this->assertSame('b202d9e2eada440c', $verification->getKeyID(true));
+        $this->assertTrue($verification->isVerified());
+    }
+
+    public function testVerifyEcBrainpoolSignedMessage()
+    {
+        $messageData = <<<EOT
+-----BEGIN PGP MESSAGE-----
+
+owGbwMvMwCEss+eC822Xo9cYTwsnMaQU8Bh4pObk5CsEeAQoBLhzdZSyMAhzMMiK
+KbKw/XvMEet2h6GG79AHmCZWJpAOBi5OAZjIfmeG/2UXlWVvvuz0ebJvv0LQYYXP
+X2NXOEjMr1qhGTSv74fzjTcMf3jbFFOOi1zPX3QpdULoC6NJ6xLePdh4LrSxsGTV
+v2OXGRgB
+=SqQw
+-----END PGP MESSAGE-----
+EOT;
+
+        $publicKey = PublicKey::fromArmored(
+            file_get_contents('tests/Data/EcBrainpoolPublicKey.asc')
+        );
+        $message = LiteralMessage::fromArmored($messageData);
+        $this->assertSame(self::LITERAL_TEXT, trim($message->getLiteralData()->getData()));
+        $this->assertTrue($message->getSignature() instanceof Signature);
+
+        $verification = $message->verify([$publicKey])[0];
+        $this->assertSame('1cbcd043db44c5d6', $verification->getKeyID(true));
+        $this->assertTrue($verification->isVerified());
+    }
+
+    public function testVerifyEcCurve25519CleartextSignedMessage()
+    {
+        $messageData = <<<EOT
+-----BEGIN PGP MESSAGE-----
+
+owGbwMvMwCW2979wYMLRLG7G08JJDCkFPGkeqTk5+QoBHgEKAe5cHaUsDGJcDLJi
+iiwyjmKvtSPOL5x2tPY2TBMrE0gHAxenAEykIZ2R4e1X2xksp8+WmW3Yf2hOdWTt
+6p91S3T++H9sCzP+vMjrRzLD//Br7RNCWm4rMlWdaPr6d7rWZNVPKUfbGt6VfKtv
+fB/SzQ4A
+=/04O
+-----END PGP MESSAGE-----
+EOT;
+
+        $publicKey = PublicKey::fromArmored(
+            file_get_contents('tests/Data/EcCurve25519PublicKey.asc')
+        );
+        $message = LiteralMessage::fromArmored($messageData);
+        $this->assertSame(self::LITERAL_TEXT, trim($message->getLiteralData()->getData()));
+        $this->assertTrue($message->getSignature() instanceof Signature);
+
+        $verification = $message->verify([$publicKey])[0];
+        $this->assertSame('bdff135160c56a0b', $verification->getKeyID(true));
+        $this->assertTrue($verification->isVerified());
+    }
+}
