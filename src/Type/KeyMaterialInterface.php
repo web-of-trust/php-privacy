095fcd073bd876650846bdb44e1e0d5d676ed1cb
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2023-06-06 16:02:17 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Type/KeyMaterialInterface.php
+++ src/Type/KeyMaterialInterface.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 /**
- * This file is part of the PHP PG library.
- *
- * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ * This file is part of the PHP Privacy project.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
