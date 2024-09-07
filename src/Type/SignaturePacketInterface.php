fa7b1a4f9154f6571f14a37a4525ad4af39d71dc
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-09-07 08:52:02 +0700

    Refactor get notation data of signature packet
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Type/SignaturePacketInterface.php
+++ src/Type/SignaturePacketInterface.php
@@ -177,11 +177,11 @@ interface SignaturePacketInterface extends PacketInterface
     function getIssuerKeyID(bool $toHex = false): string;
 
     /**
-     * Get notation data sub packet
+     * Get notation data sub packets
      *
-     * @return SubpacketInterface
+     * @return array
      */
-    function getNotationData(): ?SubpacketInterface;
+    function getNotations(): array;
 
     /**
      * Get preferred hash algorithms sub packet
