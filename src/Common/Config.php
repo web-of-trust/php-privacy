cb0135c2df5c13c6a8b71357206e30a655a7ca9e
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-09-17 13:49:34 +0700

    Change SALT_NOTATION
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Common/Config.php
+++ src/Common/Config.php
@@ -31,7 +31,7 @@ final class Config
     const VERSION = 'PHP Privacy v1.1.8';
     const COMMENT = 'https://github.com/web-of-trust/php-privacy';
 
-    const SALT_NOTATION = 'PHP Privacy Salt Notation';
+    const SALT_NOTATION = 'salt@notation.weboftrust.org';
 
     private static HashAlgorithm $preferredHash = HashAlgorithm::Sha256;
 
