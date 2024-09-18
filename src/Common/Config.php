8e1c2850cda3cd971c89b8b1265b3fd2b9060425
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-09-18 09:38:51 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Common/Config.php
+++ src/Common/Config.php
@@ -37,7 +37,7 @@ final class Config
     const PADDING_MIN = 16;
     const PADDING_MAX = 32;
 
-    const SALT_NOTATION = 'salt@notation.weboftrust.org';
+    const SALT_NOTATION = 'salt@weboftrust.org';
 
     const AEAD_CHUNK_SIZE_MIN = 10;
     const AEAD_CHUNK_SIZE_MAX = 16;
