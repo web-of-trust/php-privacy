eeb34e17764d5be9d9017f8e1e00d6474fbe89db
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-09-25 11:20:12 +0700

    Fix armor text dash escape
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Common/Armor.php
+++ src/Common/Armor.php
@@ -221,7 +221,7 @@ final class Armor
             ArmorType::SignedMessage => [
                 self::SIGNED_MESSAGE_BEGIN,
                 "Hash: $hashAlgo" . Helper::EOL . Helper::EOL,
-                preg_replace('/^- /m', '- -', $text) . Helper::EOL,
+                preg_replace('/^- /m', '- - ', $text) . Helper::EOL,
                 self::SIGNATURE_BEGIN,
                 self::addHeader($customComment) . Helper::EOL,
                 chunk_split(Strings::base64_encode($data), self::TRUNK_SIZE, Helper::EOL),
