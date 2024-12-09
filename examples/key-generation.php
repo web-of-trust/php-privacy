1f5521489fe54281ed71d026dba34f5be5e3544e
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-12-09 10:55:39 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- examples/key-generation.php
+++ examples/key-generation.php
@@ -10,21 +10,21 @@ $userIDs = [
     "Nguyen Van Nguyen <nguyennv1981@gmail.com>",
     "Nguyen Van Nguyen <nguyennv@iwayvietnam.com>",
 ];
-$passphase = Helper::generatePassword();
-echo "Generate passphase: {$passphase}" . PHP_EOL;
+$passphrase = Helper::generatePassword();
+echo "Generate passphrase: {$passphrase}" . PHP_EOL;
 
 echo "Generate RSA private key" . PHP_EOL;
-$privateKey = OpenPGP::generateKey($userIDs, $passphase, KeyType::Rsa);
+$privateKey = OpenPGP::generateKey($userIDs, $passphrase, KeyType::Rsa);
 echo $privateKey->armor() . PHP_EOL;
 
 echo "Generate Ecc private key" . PHP_EOL;
-$privateKey = OpenPGP::generateKey($userIDs, $passphase, KeyType::Ecc);
+$privateKey = OpenPGP::generateKey($userIDs, $passphrase, KeyType::Ecc);
 echo $privateKey->armor() . PHP_EOL;
 
 echo "Generate Curve25519 private key" . PHP_EOL;
-$privateKey = OpenPGP::generateKey($userIDs, $passphase, KeyType::Curve25519);
+$privateKey = OpenPGP::generateKey($userIDs, $passphrase, KeyType::Curve25519);
 echo $privateKey->armor() . PHP_EOL;
 
 echo "Generate Curve448 private key" . PHP_EOL;
-$privateKey = OpenPGP::generateKey($userIDs, $passphase, KeyType::Curve448);
+$privateKey = OpenPGP::generateKey($userIDs, $passphrase, KeyType::Curve448);
 echo $privateKey->armor() . PHP_EOL;
